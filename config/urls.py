a169538d05434d32aaa9d67e6a24eb550bef30a1
sakura <151740387+amelliinn@users.noreply.github.com>
2026-03-15 22:54:47 +0700

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,11 @@
 URL configuration for config project.
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from gallery.views import home, about, upload
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,5 +15,12 @@ urlpatterns = [
     path('upload/', upload, name='upload'),
 ]
 
-# Добавляем маршруты для медиа-файлов (работает при DEBUG=False)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Добавляем маршруты для медиа-файлов (для production)
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
